00d784b823a9f5e7624b782b857b1b6848cec6ae
Luccas Pereira Nunes <93996940+LuccasLpn@users.noreply.github.com>
2025-02-26 17:40:32 -0300

    Update HiperCreditCard.java
    adicionando bins para o hipercard

--- src/main/java/br/com/moip/creditcard/HiperCreditCard.java
+++ src/main/java/br/com/moip/creditcard/HiperCreditCard.java
@@ -6,8 +6,9 @@ import java.util.TreeSet;
 
 public class HiperCreditCard {
 
-    private static Set<String> HIPER_BINS = new TreeSet<String>( Arrays.asList(
-            "637095", "637612", "637599", "637609", "637568"
+   private static final Set<String> HIPER_BINS = new TreeSet<>(Arrays.asList(
+            "637095", "637612", "637599", "637609", "637568",
+            "384100", "384140", "384160", "606282"
     ));
 
     public static boolean isBrandHiper(final String number) {
